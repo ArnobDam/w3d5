0cdf685877a5ade7da7f14cec4402d4a65cc8de0
Gleb Mirzayev <Your Email>
2022-09-30 12:20:50 -0400

    create add child method

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -1,3 +1,26 @@
 class PolyTreeNode
+    attr_reader(:parent, :children, :value)
+    def initialize(value)
+        @value = value
+        @parent = nil
+        @children = []
+    end
 
+    def parent=(parent_node)
+        if @parent != nil
+            @parent.children.delete(self)
+        end
+
+        @parent = parent_node
+        @parent.children << self unless @parent == nil
+    end
+
+    def add_child(child_node)
+        child_node.parent=self
+    end
+
+    def remove_child(child_node)
+
+    end
 end
+
